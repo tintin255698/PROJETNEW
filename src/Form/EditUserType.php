45aec1b79f6e6742f3e77a52a44e6f3e7f32536a
tintin <vivien.joly@hotmail.fr>
2020-07-20 11:27:07 +0200

    DGFFDGFDG

--- src/Form/EditUserType.php
+++ src/Form/EditUserType.php
@@ -2,7 +2,7 @@
 
 namespace App\Form;
 
-use App\Entity\Users;
+use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
@@ -31,7 +31,7 @@ class EditUserType extends AbstractType
     public function configureOptions(OptionsResolver $resolver)
     {
         $resolver->setDefaults([
-            'data_class' => Users::class,
+            'data_class' => User::class,
         ]);
     }
 }
